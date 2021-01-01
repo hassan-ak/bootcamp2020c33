086d3637875d77565ed2697918588f7b710e3cbf
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-01 19:40:03 +0500

    Step06

--- step06/src/pages/index.tsx
+++ step06/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby';
 import React from 'react';
 import Header from '../components/Header';
 import Layout from '../components/Layout';
@@ -8,6 +9,7 @@ export default () => {
             <Layout>
                 <Header text="This is a heading" subtext="The is the sub heading"></Header>
                 <p>This is my home page</p>
+                <Link to="/my-dynamic-page/">Dynamically Created Page At Build Time</Link>
             </Layout>
         </div>
     );
