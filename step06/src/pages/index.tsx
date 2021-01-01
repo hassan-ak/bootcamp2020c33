ed87f65f145a66b9f34720ef80931312566f905d
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-01 21:07:58 +0500

    Step08

--- step06/src/pages/index.tsx
+++ step06/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Link } from 'gatsby';
 import React from 'react';
 import Header from '../components/Header';
 import Layout from '../components/Layout';
+import Button from '@material-ui/core/Button';
 
 export default () => {
     return (
@@ -9,7 +10,11 @@ export default () => {
             <Layout>
                 <Header text="This is a heading" subtext="The is the sub heading"></Header>
                 <p>This is my home page</p>
-                <Link to="/my-dynamic-page/">Dynamically Created Page At Build Time</Link>
+                <Link to="/my-dynamic-page/">
+                    <Button variant="contained" color="primary">
+                        Dynamically Created Page At Build Time
+                    </Button>
+                </Link>
                 <br/>
                 <Link to="/app/page1/">Go to Client Page 1</Link>
             </Layout>
