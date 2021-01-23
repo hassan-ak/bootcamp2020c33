b6fd7aa95703d6dba9e7a0f1b2b9fde57842978e
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-23 20:26:32 +0500

    UpTill Step 05

---
+++ step06-08/src/pages/desc.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './desec.css'
+
+export default () => {
+    return (
+        <div className="myComponent">
+            How with component styles
+        </div>
+    );
+}
---
+++ step06-08/src/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../components/Header';
+import Layout from '../components/Layout';
+import { Link } from "gatsby";
+
+export default function home() {
+  return (
+    <div>
+      <Layout>
+        <Header text="This is a heading" subtext="The is the sub heading"></Header>
+        <p>This is my home page</p>
+        <p>Click <Link to="/desc/">here</Link> for component scoped css</p>
+      </Layout>
+    </div>
+  )
+}
