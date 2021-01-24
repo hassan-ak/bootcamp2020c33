f99d6669267e645ae673bbe7bc2632e122343c87
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-24 21:15:39 +0500

    Step 06 (programmatically create pages)
    1:- Create
    	a:- gatsby-node.js to create a function fopr creating pages on build time
    	b:- src\templates\dynamic-page.tsx to define a template for page to be created
    2:- Update
    	a:- src\pages\index.tsx to add link for new page

--- step06-08/src/pages/index.tsx
+++ step06-08/src/pages/index.tsx
@@ -10,6 +10,7 @@ export default function home() {
         <Header text="This is a heading" subtext="The is the sub heading"></Header>
         <p>This is my home page</p>
         <p>Click <Link to="/desc/">here</Link> for component scoped css</p>
+        <Link to="/my-dynamic-page/">Dynamically Created Page At Build Time</Link>
       </Layout>
     </div>
   )
